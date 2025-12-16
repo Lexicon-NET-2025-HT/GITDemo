349a666264a63aaa5b51e39336096e66d95593cf
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2025-12-16 13:21:14 +0100

    Add Person

---
+++ GITDemo/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GITDemo;
+
+internal class Person
+{
+    public int Age { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
